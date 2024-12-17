f3b8dbd4a85dd0ba26eed5087af12730c4d3facc
Ze Yan <yanze000@gmail.com>
2024-12-17 15:47:06 +0800

    site info change

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "shadowzey"
 
     workspaces {
       name = "homelab-external"
